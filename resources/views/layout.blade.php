e32edfd1b814c0da28e8287ac77ed30a7bf86172
rafael <rafaeljevi@hotmail.com>
2020-07-31 10:09:51 -0400

    arreglada rutas footer

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -160,27 +160,27 @@
     
   <footer>
       <!-- Jquery JS-->
-      <script src="{{asset('vendor/jquery-3.2.1.min.js')}}"></script>
+      <script src="/vendor/jquery-3.2.1.min.js"></script>
       <!-- Bootstrap JS-->
-      <script src="{{asset('vendor/bootstrap-4.1/popper.min.js')}}"></script>
-      <script src="{{asset('vendor/bootstrap-4.1/bootstrap.min.js')}}"></script>
+      <script src="/vendor/bootstrap-4.1/popper.min.js"></script>
+      <script src="/vendor/bootstrap-4.1/bootstrap.min.js"></script>
       <!-- Vendor JS       -->
-      <script src="{{asset('vendor/slick/slick.min.js')}}">
+      <script src="/vendor/slick/slick.min.js">
       </script>
-      <script src="{{asset('vendor/wow/wow.min.js')}}"></script>
-      <script src="{{asset('vendor/animsition/animsition.min.js')}}"></script>
-      <script src="{{asset('vendor/bootstrap-progressbar/bootstrap-progressbar.min.js')}}">
+      <script src="/vendor/wow/wow.min.js"></script>
+      <script src="/vendor/animsition/animsition.min.js"></script>
+      <script src="/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
       </script>
-      <script src="{{asset('vendor/counter-up/jquery.waypoints.min.js')}}"></script>
-      <script src="{{asset('vendor/counter-up/jquery.counterup.min.js')}}">
+      <script src="/vendor/counter-up/jquery.waypoints.min.js"></script>
+      <script src="/vendor/counter-up/jquery.counterup.min.js">
       </script>
-      <script src="{{asset('vendor/circle-progress/circle-progress.min.js')}}"></script>
-      <script src="{{asset('vendor/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
-      <script src="{{asset('vendor/chartjs/Chart.bundle.min.js')}}"></script>
-      <script src="{{asset('vendor/select2/select2.min.js')}}"></script>
+      <script src="/vendor/circle-progress/circle-progress.min.js"></script>
+      <script src="/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+      <script src="/vendor/chartjs/Chart.bundle.min.js"></script>
+      <script src="/vendor/select2/select2.min.js"></script>
 
       <!-- Main JS-->
-      <script src="{{asset('js/main.js')}}"></script>
+      <script src="/js/main.js"></script>
       @yield('footer')
   </footer>
 </body>
